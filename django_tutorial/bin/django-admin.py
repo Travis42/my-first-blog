d1b89164dbddcbc9bc0233e5ebd86e852525f2cc
Travis Smith <travis42@gmail.com>
2017-05-13 23:10:31 -0500

    My Django Girls app, first commit

---
+++ django_tutorial/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/t42/Documents/programming/webapps/django_lesson/django_tutorial/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
