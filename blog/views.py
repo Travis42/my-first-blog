2f3e2d6bab4c008c955da8152ceb763e5ca72fb0
Travis Smith <travis42@gmail.com>
2017-05-14 18:25:48 -0500

    Converting static content to dynamic

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    # organize by published, then by date
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+                # user request, template file, things to populate the template:
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
